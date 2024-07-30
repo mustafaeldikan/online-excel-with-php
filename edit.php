104792cf123a1ea164d3f7cf91d0fce8008a4d2c
Mustafa Eldikan <mustafa.eldikan@ogr.iuc.edu.tr>
2024-07-30 15:17:04 +0300

    first commit

---
+++ edit.php
@@ -0,0 +1,219 @@
+<?php
+$rows = 10;
+$cols = 10;
+$json_file = 'data.json'; // Path to your JSON file
+
+$file_id = isset($_GET['fid']) ? intval($_GET['fid']) : 0;
+$sheet_id = isset($_GET['sid']) ? intval($_GET['sid']) : 0;
+$file_info = ['fname' => '', 'lastModified' => ''];
+$cells = [];
+$sheets = [];
+if ($file_id) {
+    $conn = connect();
+    $query = "SELECT * FROM files WHERE fid = $file_id";
+    $result = $conn->query($query);
+    if ($result->num_rows > 0) {
+        $file_info = $result->fetch_assoc();
+        $cells = loadJsonData($file_id, $sheet_id);
+    }
+    $querySheets = "SELECT sid, sname FROM sheets WHERE fid = $file_id";
+    $sheetsResult = $conn->query($querySheets);
+    if ($sheetsResult->num_rows > 0) {
+        while ($sheet = $sheetsResult->fetch_assoc()) {
+            array_push($sheets, $sheet);
+        }
+    }
+    $conn->close();
+}
+
+function connect()
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "excel";
+
+    $conn = new mysqli($servername, $username, $password, $dbname) or die("Connection failed: " . $conn->connect_error);
+    return $conn;
+}
+
+function pageHeader($heading)
+{
+    return <<<ZZZZ
+        <html><head><title>$heading</title></head><body>
+ZZZZ;
+}
+
+function pageFooter()
+{
+    return "</body></html>";
+}
+
+function loadJsonData($file_id, $sheet_id)
+{
+    global $json_file;
+    $data = [];
+
+    if (file_exists($json_file)) {
+        $json_data = file_get_contents($json_file);
+        $data = json_decode($json_data, true);
+
+        if ($data === null) {
+            $data = [];
+        }
+    }
+
+    foreach ($data as $entry) {
+        if ($entry['file_id'] == $file_id) {
+            // Ensure the 'sheets' and 'cells' keys exist
+            $sheet_data = $entry['sheets'][$sheet_id] ?? [];
+            return $sheet_data;
+        }
+    }
+    return [];
+}
+
+
+function excel($rows, $cols, $file_info, $cells, $sheets, $file_id, $sheet_id)
+{
+    $html = "<table border='1' style='width:75%'>";
+    $html .= "<tr style='text-align: center'>
+            <th colspan='" . ($cols + 1) . "' style='text-align: left; padding-left: 10px;'><strong>File Name:</strong> " . htmlspecialchars($file_info['fname']) . "</th>
+            </tr>";
+    $html .= "<tr style='text-align: center'>
+            <th colspan='" . ($cols + 1) . "' style='text-align: left; padding-left: 10px;'><strong>Last Modified:</strong> " . htmlspecialchars($file_info['lastModified']) . "</th>
+            </tr>";
+    $html .= "<tr style='text-align: center'>
+            <th><a href='excel.php' class='action-link' style='color: blue'>&lt;=Back</a></th>";
+
+    for ($j = 1; $j <= $cols; $j++) {
+        $html .= "<th>" . chr(64 + $j) . "</th>"; // Column letters
+    }
+
+    $html .= "</tr>";
+
+    for ($i = 1; $i <= $rows; $i++) {
+        $html .= "<tr>";
+        $html .= "<td style='text-align: center;'>$i</td>";
+
+        for ($j = 1; $j <= $cols; $j++) {
+            $cell_key = "cell_{$i}_{$j}";
+            // Use null coalescing operator to avoid undefined key issue
+            $value = isset($cells[$cell_key]) ? htmlspecialchars($cells[$cell_key]) : '';
+            $html .= "<td><input type='text' name='$cell_key' data-row='$i' data-col='$j' value='$value' onchange='updateCell(this)' style='width:100%' /></td>";
+        }
+        $html .= "</tr>";
+    }
+
+    $html .= "<tr><td style='text-align: center;'><strong>Sheets</strong></td>";
+    foreach ($sheets as $sheet) {
+        $sname = $sheet['sname'];
+        $sid = $sheet['sid'];
+        $html .= "<td style='text-align: center;'>
+            <a href='edit.php?fid=$file_id' class='action-link' style='color: blue'>sheet0</a>
+        </td>";
+        $html .= "<td style='text-align: center;'>
+            <a href='edit.php?fid=$file_id&sid=$sid' class='action-link' style='color: blue'>$sname</a>
+        </td>";
+    }
+    $html .= "</tr></table>";
+
+    return $html;
+}
+
+
+//json file operations
+function saveToJsonFile($file_id, $sheet_id, $row, $col, $value)
+{
+    global $json_file;
+    $data = [];
+
+    if (file_exists($json_file)) {
+        $json_data = file_get_contents($json_file);
+        $data = json_decode($json_data, true);
+
+        if ($data === null) {
+            $data = [];
+        }
+    }
+
+    $found = false;
+    foreach ($data as &$entry) {
+        if ($entry['file_id'] == $file_id) {
+            if (!isset($entry['sheets'][$sheet_id])) {
+                $entry['sheets'][$sheet_id] = [];
+            }
+            $entry['sheets'][$sheet_id]["cell_{$row}_{$col}"] = $value;
+            $found = true;
+            break;
+        }
+    }
+
+    if (!$found) {
+        $data[] = [
+            'file_id' => $file_id,
+            'file_info' => ['fname' => '', 'lastModified' => ''],
+            'sheets' => [
+                $sheet_id => ["cell_{$row}_{$col}" => $value]
+            ]
+        ];
+    }
+
+    file_put_contents($json_file, json_encode($data, JSON_PRETTY_PRINT));
+}
+
+
+
+// Handle the POST request to update the JSON file
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $file_id = isset($_POST['file_id']) ? intval($_POST['file_id']) : 0;
+    $sheet_id = isset($_POST['sheet_id']) ? intval($_POST['sheet_id']) : 0;
+    $row = isset($_POST['row']) ? intval($_POST['row']) : 0;
+    $col = isset($_POST['col']) ? intval($_POST['col']) : 0;
+    $value = isset($_POST['value']) ? $_POST['value'] : '';
+
+    saveToJsonFile($file_id, $sheet_id, $row, $col, $value);
+
+    echo json_encode(['status' => 'success']);
+    exit;
+}
+
+
+echo '
+    ' . pageHeader("Edit File") . '
+    <h2>Edit File fid: ' . htmlspecialchars($file_id) . '</h2>
+    <form method="POST" action="#">
+        ' . excel($rows, $cols, $file_info, $cells, $sheets, $file_id, $sheet_id) . '
+    </form>
+    ' . pageFooter() . '
+    <script>
+        function updateCell(input) {
+            var row = input.getAttribute("data-row");
+            var col = input.getAttribute("data-col");
+            var value = input.value;
+            var fileId = ' . $file_id . ';
+            var sheetId = ' . $sheet_id . ';
+
+            fetch("", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: new URLSearchParams({
+                    file_id: fileId,
+                    row: row,
+                    col: col,
+                    value: value,
+                    sheet_id: sheetId
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Update successful:", data);
+            })
+            .catch(error => {
+                console.error("Error updating cell:", error);
+            });
+        }
+    </script>
+';
---
+++ excel.php
@@ -0,0 +1,133 @@
+<?php
+$conn = connect();
+
+function connect()
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "excel";
+
+    $conn = new mysqli($servername, $username, $password, $dbname) or die("Connection failed: " . $conn->connect_error);
+    return $conn;
+}
+
+switch (isset($_GET['is']) ? $_GET['is'] : '') {
+    case 'add_new_file':
+        add_new_file($_GET['fname'], $_GET['lastModified']);
+        break;
+        // case 'excel':
+        //     excel($_GET['rows'], $_GET['cols'],$_GET['file_info']);
+        //     break;
+    default:
+        echo pageHeader("Student List");
+        listele();
+        echo pageFooter();
+}
+
+function pageHeader($heading)
+{
+    return <<<ZZZZ
+        <html><head><title>$heading</title>
+        <script>
+       function addRow() {
+    var fname = document.getElementById("fname").value;
+    var lastModified = document.getElementById("lastModified").value;
+    fetch('excel.php?is=add_new_file&fname=' + encodeURIComponent(fname) + '&lastModified=' + encodeURIComponent(lastModified))
+    .then(res => res.json())
+    .then(body => {
+        var table = document.getElementById("file_name").getElementsByTagName('tbody')[0];
+        var newRow = table.insertRow();
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        var cell3 = newRow.insertCell(2);
+        cell1.innerHTML = body.data.fname;
+        cell2.innerHTML = body.data.lastModified;
+        cell3.innerHTML = "<a href='edit.php?fid=" + body.data.fid + "' class='action-link'>Edit</a>";
+        document.getElementById("fname").value = "";
+        document.getElementById("lastModified").value = "";
+    })
+    .catch(err => {
+        console.log("Error:", err);
+    });
+}
+
+        </script>
+        </head><body>
+ZZZZ;
+}
+
+function pageFooter()
+{
+    return "</body></html>";
+}
+
+function add_new_file($fname, $lastModified)
+{
+    global $conn;
+    $conn = connect();
+    $fname = $conn->real_escape_string($fname);
+    $lastModified = $conn->real_escape_string($lastModified);
+    $query = "INSERT INTO files(fname, lastModified) VALUES ('$fname', CURRENT_TIMESTAMP)";
+    if ($conn->query($query) === TRUE) {
+        $fid = mysqli_insert_id($conn);
+        $body = [
+            "message" => "File successfully added.",
+            "data" => [
+                "fid" => $fid,
+                "fname" => $fname,
+                "lastModified" => $lastModified,
+            ]
+        ];
+        $query2 = "INSERT INTO sheets(sname, `rows`, cols, fid) VALUES ('sheet2', 10, 10, $fid)";
+        $conn->query($query2);
+        echo json_encode($body);
+    } else {
+        echo json_encode(["error" => "Insertion error: " . $conn->error]);
+    }
+}
+
+function listele()
+{
+    global $conn;
+    $query = 'SELECT * FROM files';
+
+    echo "<style>
+       
+        td, th {
+            border: 1px solid red;
+            padding: 8px;
+            text-align: center;
+        }
+    </style>
+    <table id='file_name'>
+        <tr>
+            <td colspan='3' class='title-cell'>My drive</td>
+        </tr>
+        <tr class='header-row'>
+            <td>File Name</td>
+            <td>Last Modified</td>
+            <td>Actions</td>
+        </tr>
+        <tr>
+            <td><input type='text' id='fname' name='fname' required></td>
+            <td><input type='date' id='lastModified' name='lastModified' required></td>
+            <td><button class='action-button' onclick='addRow()'>Add</button></td>
+        </tr>";
+
+
+    $result = $conn->query($query);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr id='{$row['fid']}'>
+                        <td>{$row['fname']}</td>
+                        <td>{$row['lastModified']}</td>
+                        <td><a href='edit.php?fid={$row['fid']}' class='action-link'>Edit</a> </td>
+                  </tr>";
+        }
+    } else {
+        echo "<tr><td colspan='3'>0 results</td></tr>";
+    }
+    echo "</table>";
+    $conn->close();
+}
